c2f1a01bc4bb314d6081efa3006b3764aa64c4aa
acehinnnqru <acehinnnqru@gmail.com>
2024-08-30 15:25:42 +0800

    feat: add node_20

---
+++ node_v20/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            libiconv
+            gcc
+
+            nodejs_20
+            yarn
+          ] ++ lib.optionals stdenv.isDarwin (with darwin;
+            with apple_sdk.frameworks; [
+              libiconv
+              libresolv
+              Libsystem
+              SystemConfiguration
+              Security
+              CoreFoundation
+            ]);
+        };
+      }
+    );
+}
