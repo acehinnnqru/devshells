2665e34292da70de1f664a09b8369a29d02241f8
acehinnnqru <acehinnnqru@gmail.com>
2024-09-26 16:22:12 +0800

    feat: add go_1_20

---
+++ go_1_20/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            libiconv
+            gcc
+
+            # go contains go and gofmt
+            go_1_21
+
+            golangci-lint
+            gopls
+            gotools
+          ] ++ lib.optionals stdenv.isDarwin (with darwin;
+            with apple_sdk.frameworks; [
+              libiconv
+              libresolv
+              Libsystem
+              SystemConfiguration
+              Security
+              CoreFoundation
+            ]);
+        };
+      }
+    );
+}
